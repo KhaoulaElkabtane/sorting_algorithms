8b1d09c3cae9e5d764179413db147ac170250f31
KhaoulaElkabtane <elkabtanekhaoula@gmail.com>
2023-10-23 15:32:57 -0700

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,11 +13,11 @@ void quick_sort(int *array, size_t size);
  */
 void swap_ints(int *s, int *i)
 {
-        int tm;
+	int tm;
 
-        tm = *s;
-        *s = *i;
-        *i = tm;
+	tm = *s;
+	*s = *i;
+	*i = tm;
 }
 
 /**
